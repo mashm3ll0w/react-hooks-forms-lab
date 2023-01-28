c91bf538b383a919e1ce3516118e5d557b9bb804
Charles Swaleh <charlswall@live.com>
2023-01-29 02:27:48 +0300

    Completed the search product by name filter

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,6 +5,11 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("")
+
+  function handleSearch(e){
+    setSearch(e.target.value)
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -16,12 +21,21 @@ function ShoppingList({ items }) {
     return item.category === selectedCategory;
   });
 
+  const searchItems = itemsToDisplay.filter(item => {
+    if (search.length === 0){
+      return true
+    }
+    else{
+      return item.name.includes(search[0].toUpperCase() + search.slice(1))
+    }
+  })
+
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearch} search={search}/>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {searchItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
